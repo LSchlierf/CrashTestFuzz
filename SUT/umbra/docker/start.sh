5ee85ad35853e0f07bb3d6376235236ee86868d5
Lucas Schlierf <LucasSchlierf@gmail.com>
2025-02-03 15:46:47 +0100

    fixed lazyfs config

--- SUT/umbra/docker/start.sh
+++ SUT/umbra/docker/start.sh
@@ -1,8 +1,8 @@
 CRASHCMD=${CRASHCMD:=""}
 
-echo "#begin custom cmd" >> /lazyfs/lazyfs/config/default.toml && \
-echo "$CRASHCMD" >> /lazyfs/lazyfs/config/default.toml && \
-echo "#end custom cmd" >> /lazyfs/lazyfs/config/default.toml && \
-cd /lazyfs/lazyfs && ./scripts/mount-lazyfs.sh -c /lazyfs/lazyfs/config/default.toml -m /var/db -r /tmp/lazyfs.root && \
+echo "#begin custom cmd" >> /lazyfs/lazyfs/config/config.toml && \
+echo "$CRASHCMD" >> /lazyfs/lazyfs/config/config.toml && \
+echo "#end custom cmd" >> /lazyfs/lazyfs/config/config.toml && \
+cd /lazyfs/lazyfs && ./scripts/mount-lazyfs.sh -c /lazyfs/lazyfs/config/config.toml -m /var/db -r /tmp/lazyfs.root && \
 sleep 1 && \
 script -qfc /docker-entrypoint.sh /tmp/umbra.log & tail -f /dev/null
