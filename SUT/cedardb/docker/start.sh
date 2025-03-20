7f84a55b5339174674525f38bd93a8937a3f4314
Lucas Schlierf <lucas.schlierf@tum.de>
2025-03-20 15:45:38 +0100

    fixed lazyfs startup script

--- SUT/cedardb/docker/start.sh
+++ SUT/cedardb/docker/start.sh
@@ -3,6 +3,6 @@ CRASHCMD=${CRASHCMD:=""}
 echo "#begin custom cmd" >> /lazyfs/lazyfs/config/config.toml && \
 echo "$CRASHCMD" >> /lazyfs/lazyfs/config/config.toml && \
 echo "#end custom cmd" >> /lazyfs/lazyfs/config/config.toml && \
-cd /lazyfs/lazyfs && ./scripts/mount-lazyfs.sh -c /lazyfs/lazyfs/config/config.toml -m /var/lib/cedardb/data -r /tmp/lazyfs.root >> /tmp/lazyfs.log && \
+cd /lazyfs/lazyfs && ./scripts/mount-lazyfs.sh -c /lazyfs/lazyfs/config/config.toml -m /var/lib/cedardb/data -r /tmp/lazyfs.root && \
 sleep 1 && \
 script -qfc /docker-entrypoint.sh /tmp/cedardb.log & tail -f /dev/null
