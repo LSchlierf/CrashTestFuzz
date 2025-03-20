c025eba641b0e44b70bb46c16af71c78ce3c01d1
Lucas Schlierf <lucas.schlierf@tum.de>
2025-03-20 16:44:24 +0100

    increased lazyfs startup buffer

--- SUT/duckdb/docker/start.sh
+++ SUT/duckdb/docker/start.sh
@@ -4,5 +4,5 @@ echo "#begin custom cmd" >> /lazyfs/lazyfs/config/config.toml && \
 echo "$CRASHCMD" >> /lazyfs/lazyfs/config/config.toml && \
 echo "#end custom cmd" >> /lazyfs/lazyfs/config/config.toml && \
 cd /lazyfs/lazyfs && ./scripts/mount-lazyfs.sh -c /lazyfs/lazyfs/config/config.toml -m /var/db -r /tmp/lazyfs.root && \
-sleep 1 && \
+sleep 3 && \
 script -qfc /server.py /tmp/duckdb.log & tail -f /dev/null
