a5adcdd1da257a6aa130f9beb715afde6adfa780
Lucas Schlierf <lucas.schlierf@tum.de>
2025-04-02 17:19:43 +0200

    error in umbra setup

--- SUT/umbra-assert/docker/docker-entrypoint.sh
+++ SUT/umbra-assert/docker/docker-entrypoint.sh
@@ -13,9 +13,7 @@ env
 # Start the server
 echo "Starting the server!"
 if [[ -e /var/db/umbra.cert && -e /var/db/umbra.pem ]]; then
-  WAL_SYNC_METHOD=none
   exec umbra-server -address 0.0.0.0 /var/db/umbra.db || exit 1
 else
-  WAL_SYNC_METHOD=none 
   exec umbra-server -createSSLFiles -certFile /var/db/umbra.cert -keyFile /var/db/umbra.pem -address 0.0.0.0 /var/db/umbra.db || exit 1
 fi
--- SUT/umbra/docker/docker-entrypoint.sh
+++ SUT/umbra/docker/docker-entrypoint.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# env
 
 # Setup a database
 if [[ -e /var/db/umbra.db ]]; then
@@ -13,9 +12,7 @@ env
 # Start the server
 echo "Starting the server!"
 if [[ -e /var/db/umbra.cert && -e /var/db/umbra.pem ]]; then
-  WAL_SYNC_METHOD=none
   exec umbra-server -address 0.0.0.0 /var/db/umbra.db || exit 1
 else
-  WAL_SYNC_METHOD=none 
   exec umbra-server -createSSLFiles -certFile /var/db/umbra.cert -keyFile /var/db/umbra.pem -address 0.0.0.0 /var/db/umbra.db || exit 1
 fi
