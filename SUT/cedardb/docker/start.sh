487d2006e97ff58f0141bd29604ca82b69a39b38
Lucas Schlierf <LucasSchlierf@gmail.com>
2025-05-25 23:13:26 +0200

    using built in docker-entrypoint.sh for cedardb

--- SUT/cedardb/docker/start.sh
+++ SUT/cedardb/docker/start.sh
@@ -5,4 +5,4 @@ echo "$CRASHCMD" >> /lazyfs/lazyfs/config/config.toml && \
 echo "#end custom cmd" >> /lazyfs/lazyfs/config/config.toml && \
 cd /lazyfs/lazyfs && ./scripts/mount-lazyfs.sh -c /lazyfs/lazyfs/config/config.toml -m /var/lib/cedardb/data -r /tmp/lazyfs.root && \
 sleep 3 && \
-script -qfc /docker-entrypoint.sh /tmp/cedardb.log & tail -f /dev/null
+script -qfc /usr/local/bin/docker-entrypoint.sh /tmp/cedardb.log & tail -f /dev/null
